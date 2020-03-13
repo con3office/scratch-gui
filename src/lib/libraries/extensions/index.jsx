41d9ec8b2ce234f5252b5d10bf0332b896ba3341
con3office <con3office@gmail.com>
2020-03-13 12:16:55 +0900

    Microbit More

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -49,6 +49,11 @@ import gdxforConnectionSmallIconURL from './gdxfor/gdxfor-small.svg';
 import pasorichImage from './pasorich/pasorich.png';
 import pasorichInsetImage from './pasorich/pasorich-small.png';
 
+import microbitMoreIconURL from './microbitMore/microbitMore.png';
+import microbitMoreInsetIconURL from './microbitMore/microbitMore-small.svg';
+import microbitMoreConnectionIconURL from './microbitMore/microbitMore-illustration.svg';
+import microbitMoreConnectionSmallIconURL from './microbitMore/microbitMore-small.svg';
+
 export default [
     {
         name: (
@@ -70,6 +75,36 @@ export default [
             />
         ),
         featured: true
+    },
+    {
+        name: 'micro:bit More',
+        extensionId: 'microbitMore',
+        collaborator: 'Yengawa Lab',
+        iconURL: microbitMoreIconURL,
+        insetIconURL: microbitMoreInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Connect your projects with the world."
+                description="Description for the 'micro:bit' extension"
+                id="gui.extension.microbit.description"
+            />
+        ),
+        featured: true,
+        disabled: false,
+        bluetoothRequired: true,
+        internetConnectionRequired: false,
+        launchPeripheralConnectionFlow: true,
+        useAutoScan: false,
+        connectionIconURL: microbitMoreConnectionIconURL,
+        connectionSmallIconURL: microbitMoreConnectionSmallIconURL,
+        connectingMessage: (
+            <FormattedMessage
+                defaultMessage="Connecting"
+                description="Message to help people connect to their micro:bit."
+                id="gui.extension.microbit.connectingMessage"
+            />
+        ),
+        helpLink: 'https://lab.yengawa.com/project/scratch-microbit-more/'
     },
     {
         name: (
